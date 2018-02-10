444415b6803bc36b14c697b0c66192ae6f1c6ee0
Ryan Collins <it@st-aidans.com>
2018-02-10 22:17:32 +0000

    Start some of the JavaScripting

--- client/chat.js
+++ client/chat.js
@@ -10,4 +10,27 @@ $(function() {
     // for chat history, load it.
   }
 
+  function addChatMessage(isClient, messageStr) {
+      let bgColour = isClient ? 'w3-teal' : 'w3-gray';
+      let imgClass = isClient ? 'w3-right w3-circle w3-margin-left' : 'w3-left w3-circle w3-margin-right';
+      let chatObject = $('<div>');
+      chatObject.addClass('w3-panel w3-round w3-border');
+      chatObject.addClass(bgColour);
+
+      let subItem = $('<img>');
+      subItem.addClass(imgClass);
+      subItem.attr('src', 'https://picsum.photos/40/40');
+      chatObject.appendChild(subItem);
+
+      subItem = null;
+      subItem = $('<span>');
+      subitem.text(messageStr);
+      chatObject.appendChild(subItem);
+      
+      // TODO: add item to DOM instead of returning
+      return chatObject;
+  }
+
+
+
 });
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Imports
 const express = require('express');
 const path = require('path');
-
 // Global Objects
 var app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 var currentDirectory = (process.env.PORT) ? process.cwd() : __dirname;
 
 app.set('port', 3000);
@@ -16,13 +17,15 @@ app.use("/libs", express.static(path.join(currentDirectory, "node_modules")));
 
 
 // Socket.io chat stuff
-
+io.on('connection', (socket) => {
+  console.log("A Connection");
+});
 
 // 404 error
 app.get("*", (req, res) => {
   res.status(404).send("file not found");
 });
 
-app.listen(app.get('port'), () => {
-  console.log("server started on port " + app.get('port'));
+http.listen(3000, () => {
+  console.log("server listening on 3000");
 });
